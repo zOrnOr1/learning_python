e7cdeb734d439635a812d823d726c25f5b7bb607
zOrnOr1 <andreywkln@gmail.com>
2022-05-24 19:05:48 +0300

    new gitignore + lesson2

--- lesson_2.py
+++ lesson_2.py
@@ -1,4 +1,6 @@
 ### GeekBrains lesson 2 + GitHub test ###
+from string import punctuation
+
 
 def lesson_2():
     # Part 1
@@ -10,10 +12,10 @@ def lesson_2():
     print("==========================================")
     # Part 2
     given_list = ['в', '5', 'часов', '17', 'минут', 'температура', 'воздуха', 'была', '+5', 'градусов']
-    signs = ['+', '-']
+
     idx = 0
     for item in range(len(given_list)):
-        if given_list[idx][0] not in signs:
+        if given_list[idx][0] not in punctuation:
             if given_list[idx].isdigit():
                 given_list[idx] = f"{int(given_list[idx]):02d}"
                 given_list.insert(idx, '"')
@@ -22,7 +24,7 @@ def lesson_2():
                 idx += 1
         else:
             if given_list[idx][1:].isdigit():
-                given_list[idx] = f"{given_list[idx][0]}{int(given_list[idx]):02d}"
+                given_list[idx] = f"{given_list[idx][0]}{int(given_list[idx][1:]):02d}"
                 given_list.insert(idx, '"')
                 idx += 1
                 given_list.insert(idx + 1, '"')
@@ -32,7 +34,7 @@ def lesson_2():
     given_list = ' '.join(given_list)
     for idx in range(len(given_list)):
         try:
-           if given_list[idx] == '"':
+            if given_list[idx] == '"':
                 if given_list[idx - 3].isdigit():
                     given_list = f'{given_list[:idx - 1]}{given_list[idx:]}'
                 if given_list[idx + 3].isdigit():
