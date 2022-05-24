3d78df570afbcea6ecc27e7e7a17fc1926c23729
zOrnOr1 <andreywkln@gmail.com>
2022-05-24 16:26:59 +0400

    + part 1

--- lesson_2.py
+++ lesson_2.py
@@ -1,7 +1,11 @@
 ### GeekBrains lesson 2 + GitHub test ###
 
 def lesson_2():
-    print("Hello World")
+    # Part 1
+    print(f"Type of 15 * 3: {type(15 * 3)}")
+    print(f"Type of 15 / 3: {type(15 / 3)}")
+    print(f"Type of 15 // 2: {type(15 // 2)}")
+    print(f"Type of 15 ** 2: {type(15 ** 2)}")
 
 
 if __name__ == "__main__":
